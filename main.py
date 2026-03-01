a1655a45781197bd689d5c621839e44e2fadd1b7
arooj-l <aliaqat@udel.edu>
2026-03-01 07:02:26 -0500

    Added few changes

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ place = input("Enter the place: ")
 animal = input("Enter the animal: ")
 adjective = input("Enter the adjective: ")
 
-story = "One day, NAME went to PLACE and saw a ANIMAL. It was very ADJECTIVE!"
+story = ("One day, NAME went to PLACE and saw a ANIMAL. "
+         "It was very ADJECTIVE! "
+         "Everyone was talking about the ANIMAL")
 
-story = story.replace("NAME", name)
-story = story.replace("PLACE", place)
-story = story.replace("ANIMAL", animal)
-story = story.replace("ADJECTIVE", adjective)
+story = story.replace("NAME", name.upper())
+story = story.replace("PLACE", place.title())
+story = story.replace("ANIMAL", animal.lower())
+story = story.replace("ADJECTIVE", adjective.lower())
 
+print("\n**** Your Mad Lib Story ****")
 print(story)
